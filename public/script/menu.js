69b72a6fb4a6147ba330bc0204c4cedbd76130d0
vfroes21 <vitinhofroes_1999@yahoo.com.br>
2022-10-10 16:04:47 -0300

    Game menu v0, music and exit

--- public/script/menu.js
+++ public/script/menu.js
@@ -1,12 +1,54 @@
 onload = function(){
-   /* let tela = document.getElementById('b');
-    tela.addEventListener('mouseover', play_bgm());*/
+    bt_m = document.getElementById('music');
+    bt_m.addEventListener('click', play_bgm);
+
+    bt_s = document.getElementById('exit');
+    bt_s.addEventListener('click', handle_exit);
+
+    let form = document.getElementById('bts');
+    form.addEventListener('submit', nEnvia);
 }
 
-/*function play_bgm()
+function nEnvia(ev)
 {
-    document.getElementById('p').style.color='red';
+    ev.preventDefault();
+}
 
-    let musica = new Audio('../audio/gameboycolor_intro.mp3');
+function play_bgm()
+{
+    musica = new Audio('../audio/Final Fantasy VII OST - Cids Theme.mp3');
+    musica.loop = true;
     musica.play();
-}*/
+
+    bt_m.removeEventListener('click', play_bgm);
+    bt_m.innerHTML = 'Pausar Música';
+    bt_m.addEventListener('click', pause_play);
+}
+
+function pause_play()
+{
+    if (bt_m.innerHTML == 'Pausar Música')
+    {
+        bt_m.innerHTML = 'Retomar Música';
+        musica.pause();
+    }
+        
+    else
+    {
+        bt_m.innerHTML = 'Pausar Música';
+        musica.play();
+    }
+}
+
+function handle_exit()
+{
+    if(confirm('Tem certeza que deseja sair?'))
+    {
+        let body = document.getElementById('b');
+
+        body.removeChild(document.getElementById('block'));
+        body.removeChild(document.getElementById('title'));
+
+        document.getElementById('exit_text').style.visibility = 'visible';
+    }
+}
